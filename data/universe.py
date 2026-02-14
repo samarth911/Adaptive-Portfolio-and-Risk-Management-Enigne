a1b26718d6479dc78832e380c1d6d42f2901dad9
syed-gufran <tazeema07@gmail.com>
2026-02-15 00:29:03 +0530

    first commit

---
+++ data/universe.py
@@ -0,0 +1,12 @@
+NIFTY50 = [
+    "RELIANCE.NS", 
+    "TCS.NS", 
+    "INFY.NS",
+    "HDFCBANK.NS", 
+    "ICICIBANK.NS", 
+    "LT.NS",
+    "SBIN.NS",
+    "ITC.NS",
+    "AXISBANK.NS",
+    "KOTAKBANK.NS"
+]
